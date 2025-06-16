07a6883dcaddf8faa8437724135dc4b8ff54a97c
Akash Yadav <ak3845yadav@gmail.com>
2025-06-16 14:27:53 +0530

    add admin backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -91,6 +91,30 @@ const registerUser = async (req, res) => {
 };
 
 // Route for user login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email == process.env.ADMIN_EMAIL &&
+      password == process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.status(200).json({
+        success: true,
+        message: "Admin logged in successfully",
+        token,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,13 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 const productRouter = express.Router();
 
 // Route to add a product
 productRouter.post(
   "/add",
+  adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -22,7 +24,7 @@ productRouter.post(
 // Route to list all products
 productRouter.get("/list", listProdcuts);
 // Route to remove a product
-productRouter.post("/remove", removeProduct);
+productRouter.post("/remove", adminAuth, removeProduct);
 // Route to get a single product by ID
 productRouter.post("/single", singleProduct);
 
